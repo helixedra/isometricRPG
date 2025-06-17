45e7e8a574275a4b475b9d4f025a124aefc294bd
helixedra <greencastle08@gmail.com>
2025-06-17 21:11:24 +0200

    dialogue upd 2

--- Scripts/DialogTrigger.cs
+++ Scripts/DialogTrigger.cs
@@ -3,14 +3,34 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public string dialogueFileName = "dialogue_npc1"; 
+    [SerializeField] private GameObject hint;
+
+    private DialogueManager dialogueManager;
+
+    void Start()
+    {
+        hint.SetActive(false);
+        dialogueManager = Object.FindFirstObjectByType<DialogueManager>();
+    }
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
         {
-            Object.FindFirstObjectByType<DialogueManager>().LoadDialogueFromFile(dialogueFileName);
+            dialogueManager.LoadDialogueFromFile(dialogueFileName);
         }
+        if (dialogueManager.dialogueUI.activeSelf)
+            hint.SetActive(false);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        hint.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        hint.SetActive(false);
+    }
 
 }
